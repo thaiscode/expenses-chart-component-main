c5cf317eaf04339cfc54efcf041bf28166c8842b
Thais Silva <thais_ap@live.com>
2022-12-28 00:11:12 -0300

    done
    all working

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-//consumindo JSON
-const dados = await ( await fetch("data.json")).json();
+//consuming json
+const data = await (await fetch("data.json")).json();
 
-//variaveis dos dias e colunas
+//day and column variables
 let column = document.querySelectorAll('.table__column')
 let dayBox = document.querySelectorAll('.table__day')
-let columnArr = Array.from(column) //começa no 0
-let dayArr = Array.from(dayBox) // começa no 0
+let columnArr = Array.from(column) 
+let dayArr = Array.from(dayBox) 
 
-/*PEGANDO O DIA DA SEMANA */
+//catching the day of the week
 let date = new Date().getDay()
-//mudando dia da semana pra começar em 0 ao invés de 1
+
+//changing day of week to start at 0 instead of 1
 transformDate()
-    
-chanceColumnColor()
-    
+
+//column height
+columnHeight()
+
+//changing column color according to current weekday
+columnColor()
+
+//defining the name of the days
 days()
-numero()
+
+//adding data
+addData()
+
+//Mouse over data
+eventListener()
 
 
 
-
-//mudando dia da semana pra começar em 0 ao invés de 1
+//changing day of week to start at 0 instead of 1
 function transformDate() {      
     /*if (date == 1) {
         date = 0
@@ -41,37 +51,51 @@ function transformDate() {
     date -= 1
 }
 
-//mudando a cor da coluna conforme dia atual da semana
-function chanceColumnColor() {
-    
+//changing column color according to current weekday
+function columnColor() {
     for (let i = 0; i <= dayArr.length; i++) {
         if (i === date) {
         columnArr[date].style.backgroundColor = "var(--cyan)"
         } 
     }
 }
-//definindo o nome dos dias
+
+//defining the name of the days
 function days() {
-    console.log(dayArr)
-    for (let i = 0; i <= 7; i++) {
-        dayArr[i].innerHTML = `${dados[i].day}`
+    for (let i = 0; i <= 6; i++) {
+        dayArr[i].innerHTML = `${data[i].day}`
     }
 }
 
-//adicionando dados
+//adding data
+function addData() {
+    let numbersHover = document.querySelectorAll('.table__data')
+    let numbersHoverArr = Array.from(numbersHover)
 
-
-function numero() {
-    let numeros = document.querySelectorAll('.table__data')
-    let numerosArr = Array.from(numeros)
-
-    for (let i = 0; i <= 7; i++) {
-        numerosArr[i].innerHTML = `$${dados[i].amount}`
+    for (let i = 0; i <= 6; i++) {
+        numbersHoverArr[i].innerHTML = `$${data[i].amount}`
     }
 }
 
-            
-        
+//column height
+function columnHeight() {
+    for (let i = 0; i <= 6; i++) {
+        columnArr[i].style.height = 
+        data[i].amount + '%'
+    }
+}
 
+//Mouse over data
+function eventListener() {
+    let box = document.querySelectorAll('.table__box')
+    let boxArr = Array.from(box) 
 
-
+    for (let i = 0; i <= 6; i++) {
+        boxArr[i].addEventListener('mouseover', function (e) {
+            e.target.children[0].classList.add('hover')
+        })
+        boxArr[i].addEventListener('mouseout', function(e) {
+            e.target.children[0].classList.remove('hover')
+        })
+    }
+}
